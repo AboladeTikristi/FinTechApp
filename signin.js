37848179f52013656adc6533add8df0ae80e6710
Abolade Tikristi <ladyj2183@gmail.com>
2022-08-29 17:47:53 +0100

    uptodate

--- signin.js
+++ signin.js
@@ -6,48 +6,15 @@
  else{
     var info=[]
  }
+
 //  Variable that deals with check number of logins without success
   var a=3;
   var i;
   var second=[];
+   alert(a)
   function myLogin(){
- 
-     console.log(info)
-       if(input1.value==""||input2.value==""){
-          alert("incomplete details");
-          return;
-      }
-      if (!info) {
-        alert("This email is not valid")
-      }
-      else{
-      for(i=0; i<=info.length; i++){
-     
-      if(input2.value!=info[i].password && input1.value!=info[i].email ){
-         a=a-1;
-        show="incorrect password";
-        
-      }
-      else if(input1.value!=info[i].email && input2.value!=info[i].password){
-        show="invalid login";
-        break;
-        
-      }
-       else  {
-        let dets={};
-        dets.accountNo=info[i].accountNo;
-        dets.email=input1.value;
-        dets.password=input2.value;
-        dets.username=info[i].username;
-        second.push(dets);
-        localStorage.setItem("Database2",JSON.stringify(second));
-        dets=JSON.parse(localStorage.getItem("Database2"));
-        
-        show="member";
-        window.location="Dashboard.html"
-      }
        // Interval for checking the time of logins  
-      if (a==0) { 
+       if (a==0) { 
        
         var s=11;
        input1.disabled=true;
@@ -83,6 +50,39 @@
            
      }
          }
+     console.log(info)
+       if(input1.value==""||input2.value==""){
+          alert("incomplete details");
+          return;
+      }
+      else{
+      for(i=0; i<=info.length; i++){
+        console.log(info[i].password)
+      if(input2.value!=info[i].password && input1.value!=info[i].email ){
+         a=a-1;
+        show="incorrect password";
+        break;
+        
+      }
+      else if(input1.value!=info[i].email && input2.value!=info[i].password){
+        show="invalid login";
+        break;
+        
+      }
+       else if(input1.value===info[i].email && input2.value===info[i].password)  {
+        let dets={};
+        dets.accountNo=info[i].accountNo;
+        dets.email=input1.value;
+        dets.password=input2.value;
+        dets.username=info[i].username;
+        second.push(dets);
+        localStorage.setItem("Database2",JSON.stringify(second));
+        dets=JSON.parse(localStorage.getItem("Database2"));
+        
+        show="member";
+        window.location="Dashboard.html"
+      }
+    
    
     }
   
